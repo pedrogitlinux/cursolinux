cf67abde26532d843fc80039074865d447fea9e3
Pedro <pedro.miranda@ucr.ac.cr>
2016-03-27 11:15:29 -0600

    Se incluyen funciones y código CASE

--- CursoAdmLinux/docs22mar/hojasExcel/src/graficar.sh
+++ CursoAdmLinux/docs22mar/hojasExcel/src/graficar.sh
@@ -2,6 +2,7 @@
 
 # Este script extrae archivos de excel y grafica datos
 
+function xlsTOcsv {
 DATOS=../hojasDatos
 
 SALIDA_DATOS=$DATOS/datos_csv
@@ -25,7 +26,36 @@ do
 	cat $archivo | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " " $5}' | grep -v Sensor | sed '1, $ s/"//g' > $SALIDA_DATOS/datos-$M.out
 
 done 2> error2.log
+}
 
 
+# Script para probar el tamaño en el disco duro.
 
+function discTest {
+espacio=`df | awk '{print $5}' | grep -v Usados | sort -n | tail -1 | cut -d "%" -f1` 
+
+case $espacio in 
+	[1-9]|[1-2]?)
+		MENSAJE="Uso bajo de almacenamiento. Tamaño = $espacio%"
+	;;
+	[3-5]?)
+		MENSAJE="Hay una partición medio llena. Tamaño = $espacio%"
+	;;
+	[6-7]?)
+		MENSAJE="El sistema pronto colapsará. Tamaño = $espacio%"
+	;;
+	[8-10]?)
+		MENSAJE="NO HAY SISTEMA DE ARCHIVOS!!! "
+esac
+
+echo "Reporte uso de disco: $MENSAJE "
+
+# echo $MENSAJE | mail -s "Reporte de espacio en disco `date`" pedro.miranda@ucr.ac.cr
+# echo $MENSAJE | mail -s "Reporte de espacio en disco `date`" sysadmin
+
+exit 0
+}
+
+discTest
+#xlsTOcsv
 
