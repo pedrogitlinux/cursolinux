656f32f842d6d9e0d6ed89aa33d3c75f549d5989
Pedro <pedro.miranda@ucr.ac.cr>
2016-03-27 20:24:43 -0600

    Se incluye un menú que se repite y permite seleccionar función a ejecutar

--- CursoAdmLinux/docs22mar/hojasExcel/src/graficar.sh
+++ CursoAdmLinux/docs22mar/hojasExcel/src/graficar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ------ INICIO DE FUNCION ------
 # Este script extrae archivos de excel y grafica datos
 
 function xlsTOcsv {
@@ -28,7 +29,7 @@ do
 done 2> error2.log
 }
 
-
+# ----- INICIO DE FUNCION -----
 # Script para probar el tamaño en el disco duro.
 
 function discTest {
@@ -50,12 +51,43 @@ esac
 
 echo "Reporte uso de disco: $MENSAJE "
 
+# LA FUNCION mail DA ERROR. ES NECESARIO CONFIGURAR
 # echo $MENSAJE | mail -s "Reporte de espacio en disco `date`" pedro.miranda@ucr.ac.cr
 # echo $MENSAJE | mail -s "Reporte de espacio en disco `date`" sysadmin
 
-exit 0
 }
 
-discTest
-#xlsTOcsv
+# ----- INICIO DE FUNCION -----
+# El siguiente código contiene el menú que se repite hasta que se elija SALIR
+
+function menu {
+echo ""
+echo "Para seleccionar una de las siguientes opciones, digite: "
+echo " 1- Para realizar un test del Disco duro "
+echo " 2- Para realizar una conversión de archivos Excel a CSV "
+echo " 0- Para SALIR " 
+echo -n "Su selección es: ";	read NUM
+echo ""
+
+case $NUM in
+	1)
+		discTest
+	;;
+	2)
+		xlsTOcsv
+	;;
+	0)
+		echo "Ha seleccionado SALIR"
+		exit 0
+	;;
+	*)
+		echo "Selección inválida. Vuelva a seleccionar "
+esac
+}
+
+OPCION=true
+while [ ${OPCION} ]
+do
+	menu
+done
 
