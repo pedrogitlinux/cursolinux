a27f3874d77830fe6120bd25cf2540f2e4eb3af3
Pedro <pedro.miranda@ucr.ac.cr>
2016-03-30 22:30:48 -0600

    Se añaden comentarios al archivo

--- CursoAdmLinux/docs22mar/hojasExcel/src/graficar.sh
+++ CursoAdmLinux/docs22mar/hojasExcel/src/graficar.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
+# Este programa contiene 3 funciones y un ciclo WHILE:
+# 1- Función "xlsTOcsv"
+# Esta función covierte archivos Excel (xls) a archivos de texto (csv) y después
+# procesa los archivos CSV para quitarle datos y dejar solo los datos útiles.
+# 2- Función "discTest"
+# Esta función lee el uso del disco duro y según el porcentaje de uso, muestra un
+# mensaje informativo o de alerta. El mensaje puede ser enviado por correo.
+# 3- Función "menu"
+# Esta función muestra un menú y captura lo que el usuario seleccione para ejecutar una
+# de las funciones anteriores o "SALIR" de la aplicación. El menú se vuelve a mostrar
+# después de cada función, o si realiza una selección inválida, hasta que seleccione "SALIR".
+# WHILE: Ejecuta el menú mientras el usuario no seleccione salir del programa.
+
+
 # ------ INICIO DE FUNCION ------
 # Este script extrae archivos de excel y grafica datos
 
 function xlsTOcsv {
+# Variables que definen los directorios donde se leerán los archivos y escribirán los nuevos
 DATOS=../hojasDatos
 
 SALIDA_DATOS=$DATOS/datos_csv
 
+# Crea directorio donde gurdar nuevos archivos
 mkdir $SALIDA_DATOS
 
+# Variable numérica que utiliza para leer el nombre de archivos y crear nuevos, la inicializa en 0
 M=0
 
+# Inicia ciclo FOR que lee archivos XLS y convierte a CSV
+# Incrementa contador, muestra mensaje, utiliza comando xls2csv y envia salida a archivo creado
 for archivo in `find $DATOS -name "*.xls"`
 do
         let M=M+1
 	echo "Procesando archivo $archivo"
 	xls2csv $archivo > $SALIDA_DATOS/datos-$M.csv
+# La información de error que se genera se envía a error1.log
 done 2>error1.log
 
+# Reseteamos la variable a 0
 M=0
+
+# Inicia el ciclo FOR que lee los archivos CSV y les quita los datos que se le indican
+# Incrementa contador, muestra mensaje, lee archivo y elimina comillas. La salida la envía a archivo
 for archivo in `find $SALIDA_DATOS -name "*.csv"`
 do
 	let M=M+1
 	echo "Dando formato al archivo de datos: $archivo"
 	cat $archivo | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " " $5}' | grep -v Sensor | sed '1, $ s/"//g' > $SALIDA_DATOS/datos-$M.out
-
+# La información de error que se genera se envía a error2.log
 done 2> error2.log
 }
 
@@ -33,8 +57,11 @@ done 2> error2.log
 # Script para probar el tamaño en el disco duro.
 
 function discTest {
+# Se define variable que guardará información de utilización de particiones del disco.
+# Se filtran datos para tomar solo el valor más alto
 espacio=`df | awk '{print $5}' | grep -v Usados | sort -n | tail -1 | cut -d "%" -f1` 
 
+# Inicia CASE que determina en función del valor obtenido, cual mensaje se mostrará
 case $espacio in 
 	[1-9]|[1-2]?)
 		MENSAJE="Uso bajo de almacenamiento. Tamaño = $espacio%"
@@ -49,9 +76,10 @@ case $espacio in
 		MENSAJE="NO HAY SISTEMA DE ARCHIVOS!!! "
 esac
 
+# Muestra mensaje
 echo "Reporte uso de disco: $MENSAJE "
 
-# LA FUNCION mail DA ERROR. ES NECESARIO CONFIGURAR
+# LA FUNCION mail DA ERROR. ES NECESARIO RECONFIGURAR
 # echo $MENSAJE | mail -s "Reporte de espacio en disco `date`" pedro.miranda@ucr.ac.cr
 # echo $MENSAJE | mail -s "Reporte de espacio en disco `date`" sysadmin
 
@@ -61,14 +89,19 @@ echo "Reporte uso de disco: $MENSAJE "
 # El siguiente código contiene el menú que se repite hasta que se elija SALIR
 
 function menu {
+
+# Se muestran las opciones al usuario
 echo ""
 echo "Para seleccionar una de las siguientes opciones, digite: "
 echo " 1- Para realizar un test del Disco duro "
 echo " 2- Para realizar una conversión de archivos Excel a CSV "
-echo " 0- Para SALIR " 
+echo " 0- Para SALIR "
+# Se captura lo que digita el usuario y se guarda en variable NUM
 echo -n "Su selección es: ";	read NUM
 echo ""
 
+# Inicia CASE que ejecuta una función, muestra un mensaje de error, o sale
+# dependiendo del valor introducido por el usuario
 case $NUM in
 	1)
 		discTest
@@ -85,7 +118,10 @@ case $NUM in
 esac
 }
 
+# Crea e inicaliza variable booleana para usar en el WHILE
 OPCION=true
+
+# Inicia ciclo que se ejecuta función "menu" mientras la variable sea verdadera
 while [ ${OPCION} ]
 do
 	menu
